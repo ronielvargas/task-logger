fd6b99c380d4119d8e261841d8f49ccfbce197af
ronielvargas <nielz619@gmail.com>
2024-05-02 10:21:15 +0800

    now responsive

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -8,21 +8,21 @@ function Tasks({ tasks, onDelete, lastAdded }) {
             <Row className="mt-5 gap-5">
                 <Col className="frontend-container">
                     <p>Front-End</p>
-                    {tasks
+                    {tasks && tasks
                         .filter((task) => task.type === "front-end")
                         .map((task) => (
                             <Subtask key={task.id} task={task} onDelete={onDelete} lastAdded={lastAdded} />
-
                         ))}
+                    {!tasks || (tasks && tasks.filter((task) => task.type === "front-end").length === 0) && <h5>No task available.</h5>}
                 </Col>
                 <Col className="backend-container">
                     <p>Back-End</p>
-                    {tasks
+                    {tasks && tasks
                         .filter((task) => task.type === "back-end")
                         .map((task) => (
                             <Subtask key={task.id} task={task} onDelete={onDelete} lastAdded={lastAdded} />
-
                         ))}
+                    {!tasks || (tasks && tasks.filter((task) => task.type === "back-end").length === 0) && <h5>No task available.</h5>}
                 </Col>
             </Row>
         </>
